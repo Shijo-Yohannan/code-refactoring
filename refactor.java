3c47f3fc472733efb5578560857091d494c7a2c1
Shijo-Yohannan <62067433+Shijo-Yohannan@users.noreply.github.com>
2020-08-18 19:19:32 +0530

    Update refactor.java

--- refactor.java
+++ refactor.java
@@ -12,33 +12,33 @@ class refactoring
 		Scanner sc=null;
 		while((s=br.readLine())!=null)
 		{
-			c++;
+			c++;						//counting lines
 			sc=new Scanner(s);
 			x=sc.next();
 			if(x.compareTo("for")==0)
 			{
 				k++;
 			}
-			if(k==2 && x.compareTo("if")==0)
+			if(k==2 && x.compareTo("if")==0)		//k==2 implies nested for loop and if condition states bubble sort algorithm pattern
 			{
-				v=sc.next();
+				v=sc.next();				//if condition contains array name and v is used to hold array name to be extracted
 				break;
 			}
 		}
 		refactoring r=new refactoring();
 		System.out.println("The Original File:\n");
-		r.display("code");
-		r.modify(r.extract(v));
+		r.display("code");					// printing original file
+		r.modify(r.extract(v));					// extracting array name and using that to refactor original code
 		System.out.println("\nThe Refactored File:\n");
-		r.display("refactor");
+		r.display("refactor");					// Printing refactored code
 	}
 
 	public String extract(String v)
 	{
 		String a="";
 		for(int i=0;i<v.length();i++)
-			if(v.charAt(i)!='[')
-				a+=v.charAt(i);
+			if(v.charAt(i)!='[')				// 'array[i]' has the array named 'array'; 'a[i]' has the array named 'a' - hence String a accepts input until '[' 
+'				a+=v.charAt(i);
 			else
 				break;
 		return a;
@@ -54,26 +54,26 @@ class refactoring
 		{
 			while(x.hasNextLine())
 			{
-				u.write(x.nextLine()+"\n");
+				u.write(x.nextLine()+"\n");		// adding quick sort as a function from file q
 			}
 			for(int i=0;i<c-3;i++)
 			{
-				u.write(f.nextLine()+"\n");
+				u.write(f.nextLine()+"\n");		// adding original code until bubble sort was encountered until 3 lines before counted lines - nested for loop and if condition
 			}
 			for(int i=0;i<4;i++)
 				if(f.hasNextLine())
-					s1=f.nextLine();
-			u.write("quicksort("+s+",0,n-1)\n");
+					s1=f.nextLine();		// moving cursor past the bubble sort
+			u.write("quicksort("+s+",0,n-1)\n");		// adding quicksort function call with extracted array name
 			while(f.hasNextLine())
 			{
-				u.write(f.nextLine()+"\n");
+				u.write(f.nextLine()+"\n");		// adding remaining part of the original code file
 			}
 		}
 		catch(FileNotFoundException e)
 		{
-			System.out.println("no file apparently");
+			System.out.println("File Missing");
 		}
-		catch(Exception e)
+		catch(Exception e)					// any other exceptions will be caught and displayed 
 		{
 			System.out.println(e);
 		}
@@ -89,7 +89,7 @@ class refactoring
 	{
 		Scanner f=new Scanner(new BufferedReader(new FileReader(s)));
 		while(f.hasNextLine())
-			System.out.println(f.nextLine());
+			System.out.println(f.nextLine());		// Displaying contents of file
 	}
 }
 
